0706230c30f6c99e91a5ef0e9a985771d09166ec
John Doe <shakilansary9@gmail.com>
2023-12-25 12:29:57 +0530

    Font Removed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,4 @@
 import "./globals.css";
-import { Space_Grotesk } from "next/font/google";
-
-const space = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Bangla Font CDN - World's Largest Free Bangla Font CDN Library",
@@ -14,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-sans">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
