42d30f8bd4ad6d0b7c021e11eaf2d0b984306576
John Doe <shakilansary9@gmail.com>
2023-12-25 12:33:04 +0530

    Font Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,6 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        sans: ["Space Grotesk", "Hind Siliguri", "sans-serif"],
         firacode: "'Fira Code', monospace",
       },
       colors: {
